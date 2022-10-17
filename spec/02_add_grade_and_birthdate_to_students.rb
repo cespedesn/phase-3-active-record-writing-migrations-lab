e1c5702a424f36337c9ba0849fdc1e800c327857
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-10-17 11:29:53 -0400

    commit tree 697ce1e09858eaaf8d9723f322ab121395defb56

---
+++ spec/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,8 @@
+# class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[6.1]
+#     def change
+#       add_column :name do |t|
+#         t.string :name
+#       end
+#     end
+#   end
+  
